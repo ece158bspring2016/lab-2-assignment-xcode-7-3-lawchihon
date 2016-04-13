99c0451c605ff701744159f88521b3f6ec9cdf8b
John <lawchihon@ymail.com>
2016-04-13 15:00:06 -0700

    Initial Commit

---
+++ Introduction-to-Storyboards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Introduction-to-Storyboards
+//
+//  Created by John Law on 13/4/2016.
+//  Copyright © 2016 Chi Hon Law. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
