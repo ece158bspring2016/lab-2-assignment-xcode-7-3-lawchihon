2ffb8c53a7b0d2e9a68cbba59bd8183baffd4745
John <lawchihon@ymail.com>
2016-04-17 17:11:55 -0700

    Designing Your Own Prototype Cells

--- Introduction-to-Storyboards/PlayersViewController.swift
+++ Introduction-to-Storyboards/PlayersViewController.swift
@@ -36,14 +36,26 @@ class PlayersViewController: UITableViewController {
         return players.count
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)
-        -> UITableViewCell {
-            let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath)
-            
-            let player = players[indexPath.row] as Player
-            cell.textLabel?.text = player.name
-            cell.detailTextLabel?.text = player.game
-            return cell
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath) //1
+        
+        let player = players[indexPath.row] as Player //2
+        
+        if let nameLabel = cell.viewWithTag(100) as? UILabel { //3
+            nameLabel.text = player.name
+        }
+        if let gameLabel = cell.viewWithTag(101) as? UILabel {
+            gameLabel.text = player.game
+        }
+        if let ratingImageView = cell.viewWithTag(102) as? UIImageView {
+            ratingImageView.image = self.imageForRating(player.rating)
+        }
+        return cell
+    }
+
+    func imageForRating(rating:Int) -> UIImage? {
+        let imageName = "\(rating)Stars"
+        return UIImage(named: imageName)
     }
 
     /*
